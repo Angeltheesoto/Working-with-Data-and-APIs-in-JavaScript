9aaf6ac3f56f48c04d9b731c82a0be949b4e3dda
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-06-06 10:35:29 -0400

    on 1.2 tabular data

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Working-with-Data-and-APIs-in-JavaScript
+
+Getting setup and ready -
+
+1. basics - html, css, javascript, vscode, gitHub, node
+
+2.
+
+3.
--- project-1/index.js
+++ project-1/index.js
@@ -1,16 +1,78 @@
 
+//  Module 1: fetch() with image, CSV, JSON
 
 
+// proj-1.0
+/*
+1) call fetch(__Path___)
+2) respond
+3) complete data stream
+4) make an <img> element with the data
+
+1) errors
+2) use async/ await instead of .then
+*/
+
+// console.log('about to fetch rainbow');
+
+// fetch('images/rainbow.jpg')
+//  .then(respond => {
+//  console.log(respond)
+//  return respond.blob();
+// })
+// .then(blob => {
+//  console.log(blob);
+//  document.getElementById('rainbow').src = URL.createObjectURL(blob);
+// })
+// .catch(error => {
+//  console.log('error!')
+//  console.log(error);
+// });
+
+// make this more readable below -
+
+// async function catchRainbow() {
+//  const response = await fetch('images/rainbow.jpg');
+//  const blob = await response.blob();
+//  document.getElementById('rainbow').src = URL.createObjectURL(blob);
+// }
+// catchRainbow()
+// .then(response => {
+//  console.log('Its a beautiful rainbow!')
+// })
+// .catch(error => {
+//  console.log('error!')
+//  console.log(error);
+// });
 
 
+// how to fetch an array of images -
 
+const filenames = [
+ 'images/rainbow.jpg',
+ 'images/rainbow2.jpg',
+ 'images/rainbow3.jpg'
+];
 
+async function catchRainbow(filenames) {
+ for(let filename of filenames) {
+ const response = await fetch(filename);
+ const blob = await response.blob();
+ const img = document.createElement('img')
+ img.src = URL.createObjectURL(blob);
+ img.width = '200';
+ document.body.append(img);
+}
+}
 
-
-
-
-
-
+catchRainbow(filenames)
+.then(response => {
+ console.log('Its a beautiful rainbow!')
+})
+.catch(error => {
+ console.log('error!')
+ console.log(error);
+});
 
 
 
